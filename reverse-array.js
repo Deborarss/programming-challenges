0bb7a2988319b8624af6d2d4fb793cfbc1417dc7
Débora <debora_rss@hotmail.com>
2020-10-17 20:32:57 -0300

    reverse array

---
+++ reverse-array.js
@@ -0,0 +1,17 @@
+/*
+Reverse an Array
+Write a function to reverse an array.
+
+Examples
+reverse([1, 2, 3, 4]) ➞ [4, 3, 2, 1]
+
+reverse([9, 9, 2, 3, 4]) ➞ [4, 3, 2, 9, 9]
+
+reverse([]) ➞ []
+*/
+
+function reverse(arr) {
+  return arr.reverse();
+}
+
+console.log(reverse([1,2,3,4]));
