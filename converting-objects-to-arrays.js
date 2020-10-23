8521cc23e3ab78db6b737123cb65112f7f6d5e25
Débora <debora_rss@hotmail.com>
2020-10-23 15:07:07 -0300

    Convert objects to arrays

---
+++ converting-objects-to-arrays.js
@@ -0,0 +1,19 @@
+/*
+Converting Objects to Arrays
+Write a function that converts an object into an array, where each element represents a key-value pair in the form of an array.
+
+Examples
+toArray({ a: 1, b: 2 }) ➞ [["a", 1], ["b", 2]]
+
+toArray({ shrimp: 15, tots: 12 }) ➞ [["shrimp", 15], ["tots", 12]]
+
+toArray({}) ➞ []
+Notes
+Return an empty array if the object is empty.
+*/
+
+function toArray(obj) {
+	return Object.entries(obj)
+}
+
+console.log(toArray({shrimp: 15, tots: 12}))
