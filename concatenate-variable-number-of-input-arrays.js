397b4da79862d3e151a7263c2e93877b064a5e35
Debora <debora_rss@hotmail.com>
2020-10-29 13:14:59 -0300

    Concatenate variable number of input arrays

---
+++ concatenate-variable-number-of-input-arrays.js
@@ -0,0 +1,21 @@
+/*
+Concatenate Variable Number of Input Arrays
+Create a function that concatenates n input arrays, where n is variable.
+
+Examples
+concat([1, 2, 3], [4, 5], [6, 7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+
+concat([1], [2], [3], [4], [5], [6], [7]) ➞ [1, 2, 3, 4, 5, 6, 7]
+
+concat([1, 2], [3, 4]) ➞ [1, 2, 3, 4]
+
+concat([4, 4, 4, 4, 4]) ➞ [4, 4, 4, 4, 4]
+Notes
+Arrays should be concatenated in order of the arguments.
+*/
+
+function concat(...args) {
+  return args.flat();
+}
+
+console.log(concat([1, 2, 3], [4, 5], [6, 7]));
