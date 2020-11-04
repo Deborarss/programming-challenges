5e0419a66a264475521c52dc6f0e914e49da45ed
Debora <debora_rss@hotmail.com>
2020-11-04 19:49:20 -0300

    Finding Nemo

---
+++ finding-nemo.js
@@ -0,0 +1,26 @@
+/*
+Finding Nemo
+You're given a string of words. You need to find the word "Nemo", and return a string like this: "I found Nemo at [the order of the word you find nemo]!".
+
+If you can't find Nemo, return "I can't find Nemo :(".
+
+Examples
+findNemo("I am finding Nemo !") ➞ "I found Nemo at 4!"
+
+findNemo("Nemo is me") ➞ "I found Nemo at 1!"
+
+findNemo("I Nemo am") ➞ "I found Nemo at 2!"
+Notes
+! , ? . are always separated from the last word.
+Nemo will always look like Nemo, and not NeMo or other capital variations.
+Nemo's, or anything that says Nemo with something behind it, doesn't count as Finding Nemo.
+If there are multiple Nemo's in the sentence, only return for the first one.
+*/
+
+function findNemo(str) {
+  const findWord = parseInt(str.split(" ").indexOf("Nemo") + 1);
+  if (findWord <= 0) return "I can't find Nemo :(";
+  return "I found Nemo at " + findWord + "!";
+}
+
+console.log(findNemo("I am finding Nemo !"));
